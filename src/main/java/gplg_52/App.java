761c3157bd47d60b919dbde7dda218adbdd1ab18
uvsq21807248 <atmanihajar09@gmail.com>
2020-05-24 16:01:08 +0200

    class main

---
+++ src/main/java/gplg_52/App.java
@@ -0,0 +1,13 @@
+package gplg_52;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
